8a65c02aab1b4fc4a413c33a94bbe3b41ba55a31
Daniel Ready <Dan Ready>
2021-12-01 08:36:46 -0500

    Day 1

---
+++ day1.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+double average(Iterable<num> nums) {
+  return nums.fold<num>(0, (a, b) => a + b) / nums.length;
+}
+
+int count_increases(List<num> depths) {
+  num previous_depth = depths[0];
+  int depth_increases = 0;
+  for (num depth in depths) {
+    if (depth > previous_depth) depth_increases++;
+    previous_depth = depth;
+  }
+  return depth_increases;
+}
+
+void main() async {
+  print(Platform.script.toFilePath());
+  print(Directory.current);
+  File input = new File('day1.txt');
+  List<String> lines = await input.readAsLines();
+  List<int> depths = lines.map((e) => int.parse(e)).toList();
+
+  // part 1
+  print(count_increases(depths));
+
+  // part 2
+
+  // this line of code creates a list of the moving 3-depth-window average
+  List<double> depth_windows = Iterable.generate(depths.length - 2, (i) => average(depths.sublist(i, i + 3))).toList();
+  print(count_increases(depth_windows));
+}
